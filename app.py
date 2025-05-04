595635d1977f7a8c062bcc29ccc878efcd3f747d
André Almeida <ajalmeida3@gmail.com>
2025-05-04 01:20:32 -0300

    construindo docs com swag_from

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flasgger import Swagger, swag_from
 from flask_jwt_extended import (
-    JWTManager, create_access_token,
-    jwt_required, get_jwt_identity
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
 )
 
 app = Flask(__name__)
@@ -11,11 +14,14 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
+Swagger(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,235 +29,116 @@ class Recipe(db.Model):
     ingredients = db.Column(db.Text, nullable=False)
     time_minutes = db.Column(db.Integer, nullable=False)
 
-@app.route('/register', methods=['POST'])
-def register_user():
-    """
-    Registra um novo usuário.
-    ---
-    parameters:
-    - in: body
-        name: body
-        required: true
-        schema:
-            type: object
-            properties:
-                username:
-                    type: string
-                password:
-                    type: string
-    responses:
-        201:
-            description: Usuário criado com sucesso.
-        400:
-            description: Usuário já existe
-    """
 
+@app.route("/register", methods=["POST"])
+@swag_from("swagger_docs/register_user.yml")
+def register_user():
     data = request.get_json()
-    if User.query.filter_by(username=data['username']).first():
+    if User.query.filter_by(username=data["username"]).first():
         return jsonify({"error": "User already exists"}), 400
-    new_user = User(username=data['username'], password=data['password'])
+    new_user = User(username=data["username"], password=data["password"])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"msg": "User created"}), 201
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
+@swag_from("swagger_docs/login.yml")
 def login():
-    """
-    Faz o login do usuário e retorna um JWT.
-    ---
-    parameters:
-    - in: body
-        name: body
-        required: true
-        schema:
-            type: object
-            properties:
-                username:
-                    type: string
-                password:
-                    type: string
-    responses;
-        200:
-            description: Login bem sucedido, retorna JWT
-        401:
-            description: Credenciais inválidas
-    """
     data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
-    if user and user.password == data['password']:
-        #Converter o ID para string
+    user = User.query.filter_by(username=data["username"]).first()
+    if user and user.password == data["password"]:
+        # Converter o ID para string
         token = create_access_token(identity=str(user.id))
         return jsonify({"access_token": token}), 200
     return jsonify({"error": "Invalid credentials"}), 401
 
-@app.route('/protected', methods=['GET'])
+
+@app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
-    current_user_id = get_jwt_identity() #retorna o 'identity' usado na criação do token
-    return jsonify({"msg": f"Usuário com ID {current_user_id} acessou a rota protegida."}), 200
+    current_user_id = (
+        get_jwt_identity()
+    )  # retorna o 'identity' usado na criação do token
+    return (
+        jsonify({"msg": f"Usuário com ID {current_user_id} acessou a rota protegida."}),
+        200,
+    )
 
-@app.route('/recipes', methods=['POST'])
+
+@app.route("/recipes", methods=["POST"])
 @jwt_required()
+@swag_from("swagger_docs/create_recipe.yml")
 def create_recipe():
-    """
-    Cria uma nova receita.
-    ---
-    security:
-        - BearerAuth: []
-    parameters:
-        - in: body
-            name: body
-            schema:
-                type: object
-                required: true
-                properties:
-                    title:
-                        type: string
-                    ingredients:
-                        type: string
-                    time_minutes:
-                        type: integer
-    responses:
-        201:
-            description: Receita criada com sucesso
-        401:
-            description: Token não fornecido ou inválido
-    """
+
     data = request.get_json()
     new_recipe = Recipe(
-        title= data['title'],
-        ingredients = data['ingredients'],
-        time_minutes= data['time_minutes']
+        title=data["title"],
+        ingredients=data["ingredients"],
+        time_minutes=data["time_minutes"],
     )
     db.session.add(new_recipe)
     db.session.commit()
     return jsonify({"msg": "Recipe created"}), 201
 
 
-@app.route('/recipes', methods=['GET'])
+@app.route("/recipes", methods=["GET"])
 @jwt_required()
+@swag_from("swagger_docs/get_recipes.yml")
 def get_recipes():
-    """
-    Lista receitas com filtros opcionais.
-    ---
-    security:
-        - BearerAuth: []
-    parameters:
-        - in: query
-            name: ingredient
-            type: string
-            required: false
-            description: Filtra por ingrediente
-        - in: query
-            name: max_time
-            type: integer
-            required: false
-            description: Tempo máximo de preparo (minutos)
-    responses:
-        200:
-            description: Lista de Receitas filtradas
-            schema:
-                type: array
-                items:
-                    type: object
-                    properties:
-                        id:
-                            type: integer
-                        title:
-                            type: string
-                        time_minutes:
-                            type: integer
-    """
-    ingredient = request.args.get('ingredient')
-    max_time = request.args.get('max_time', type=int)
+
+    ingredient = request.args.get("ingredient")
+    max_time = request.args.get("max_time", type=int)
 
     query = Recipe.query
     if ingredient:
         query = query.filter(Recipe.ingredients.ilike(f"%{ingredient}%"))
     if max_time:
-        query = query. filter(Recipe.time_minutes <= max_time)
+        query = query.filter(Recipe.time_minutes <= max_time)
 
     recipes = query.all()
-    return jsonify([
-        {
-            "id": r.id,
-            "title": r.title,
-            "ingredients": r.ingredients,
-            "time_minutes": r.time_minutes
-        }
-        for r in recipes
-    ])
+    return jsonify(
+        [
+            {
+                "id": r.id,
+                "title": r.title,
+                "ingredients": r.ingredients,
+                "time_minutes": r.time_minutes,
+            }
+            for r in recipes
+        ]
+    )
 
-@app.route('/recipes/<int:recipe_id>', methods=['PUT'])
+
+@app.route("/recipes/<int:recipe_id>", methods=["PUT"])
 @jwt_required()
+@swag_from("swagger_docs/update_recipe.yml")
 def update_recipe(recipe_id):
-    """
-    Atualiza uma receita existente.
-    ---
-    security:
-        - BearerAuth: []
-    parameters:
-        - in: path
-            name: recipe_id
-            required: true
-            type: integer
-        - in: body
-            name: body
-            schema:
-                type: object
-                properties:
-                    title:
-                        type: string
-                    ingredients:
-                        type: string
-                    time_minutes:
-                        type: integer
-    responses:
-        200:
-            description: Receita atualizada
-        404:
-            description: Receita não encontrada
-        401:
-            description: Token não fornecido ou inválido
-    """
+
     data = request.get_json()
     recipe = Recipe.query.get_or_404(recipe_id)
-    if 'title' in data:
-        recipe.title = data['title']
-    if 'ingredients' in data:
-        recipe.ingredients = data['ingredients']
-    if 'time_minutes' in data:
-        recipe.time_minutes = data['time_minutes']
-    
+    if "title" in data:
+        recipe.title = data["title"]
+    if "ingredients" in data:
+        recipe.ingredients = data["ingredients"]
+    if "time_minutes" in data:
+        recipe.time_minutes = data["time_minutes"]
+
     db.session.commit()
     return jsonify({"msg": "Recipe updated"}), 200
 
-@app.route('/recipes/<int:recipe_id>', methods=['DELETE'])
+
+@app.route("/recipes/<int:recipe_id>", methods=["DELETE"])
 @jwt_required()
+@swag_from("swagger_docs/delete_recipe.yml")
 def delete_recipe(recipe_id):
-    """
-    Deleta uma receita existente.
-    ---
-    security:
-        - BearerAuth: []
-    parameters:
-        - in: path
-            name: recipe_id
-            required: true
-            type: integer
-    responses:
-        200:
-            description: Receita deletada
-        404:
-            description: Receita não encontrada
-        401:
-            description: Token não fornecido ou inválido
-    """
+
     recipe = Recipe.query.get_or_404(recipe_id)
 
     db.session.delete(recipe)
     db.session.commit()
     return jsonify({"msg": "Recipe deleted"}), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
